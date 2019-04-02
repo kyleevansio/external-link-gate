9a78b4e1ff892754a5619224c637291be5f25b4b
Kyle Evans <kyle@ran.ge>
2019-04-02 22:27:45 +0200

    Plugin header + blank index

---
+++ external-link-gate.php
@@ -0,0 +1,8 @@
+<?php
+/*
+ * Plugin Name: External Link Gate
+ * Description: Create a warning for third party links
+ * Version: 0.0.1
+ * Author: Kyle Evans
+ * Author URI: https://kyleevans.io
+ */
